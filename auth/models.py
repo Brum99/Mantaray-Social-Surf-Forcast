b2747a969dbe23997e34e68d4a9ca4571eff4a2b
Samuel David Peterson <s3722250@student.rmit.edu.au>
2024-09-25 13:32:45 +1000

    Getting ready to implement the dynamic visualisations. Showcase Images for the current state of the project.

---
+++ auth/models.py
@@ -0,0 +1,47 @@
+from utils.database import get_db_connection
+from flask_login import UserMixin
+
+class User(UserMixin):
+    def __init__(self, user_id, username, email, password):
+        self.id = user_id
+        self.username = username
+        self.email = email
+        self.password = password
+
+    @staticmethod
+    def get(user_id):
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT user_id, username, email, password FROM users WHERE user_id = %s", (user_id,))
+        user_data = cursor.fetchone()
+        cursor.close()
+        connection.close()
+
+        if user_data:
+            return User(*user_data)
+        return None
+
+    @staticmethod
+    def find_by_username(username):
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT user_id, username, email, password FROM users WHERE username = %s", (username,))
+        user_data = cursor.fetchone()
+        cursor.close()
+        connection.close()
+
+        if user_data:
+            return User(*user_data)
+        return None
+
+    @staticmethod
+    def create(username, email, password):
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", 
+                       (username, email, password))
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+
